d4274a718a658a9b1ccd25065f898a8ec763e085
Deric Kramer <deric.kramer@icanbwell.com>
2024-07-12 08:44:16 -0600

    0.0.2

--- Package.swift
+++ Package.swift
@@ -9,15 +9,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PublicPackage",
-            targets: ["PublicPackage"]),
+            targets: ["TestFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "PublicPackage"),
-        .testTarget(
-            name: "PublicPackageTests",
-            dependencies: ["PublicPackage"]),
+//        .target(
+//            name: "PublicPackage"),
+//        .testTarget(
+//            name: "PublicPackageTests",
+//            dependencies: ["PublicPackage"]),
+        .binaryTarget(
+            name: "TestFramework",
+            path: "./Sources/TestFramework.xcframework")
     ]
 )
